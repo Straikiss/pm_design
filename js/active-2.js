d1f90dd09685d83c9dad4c0c99ad8a511fa6f749
Alexey Kosenkov <starko.web@gmail.com>
2021-01-26 21:59:44 +0300

    Delete active-2.js

--- js/active-2.js
+++
@@ -1,9 +0,0 @@
-var btnContainer, btn = (btnContainer = document.getElementById("Buttons")).getElementsByClassName("create-company-change-type-2");
-
-for(var i = 0; i < btn.length; i++){
-    btn[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("create-company-change-type-active-2");
-    current[0].className = current[0].className.replace(" create-company-change-type-active-2", "");
-    this.className += " create-company-change-type-active-2";
-  });
-}
