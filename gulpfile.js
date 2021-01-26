2869e25d6bbe96941d6111977ec88be15fbf2e0b
Alexey Kosenkov <starko.web@gmail.com>
2021-01-26 21:57:26 +0300

    2.2.3
    Добавлен автоматический и про режим при создании рекламной кампании
    Внесены правки на странице настроек

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,8 @@
 var gulp = require("gulp");
-var {series} = require("gulp");
 var sass = require("gulp-sass");
 var cssnano = require("gulp-cssnano");
 var htmlmin = require("gulp-htmlmin");
 var pug = require("gulp-pug");
-var concat = require("gulp-concat")
-var minify = require("gulp-minify")
 var browserSync = require("browser-sync").create();
 
 function SassToCss(){
@@ -23,26 +20,8 @@ function PugToHtml(){
     gulp
       .src("pm/pug/*.pug")
       .pipe(pug({pretty: true}))
-      .pipe(htmlmin({collapseWhitespace: true}))
+      // .pipe(htmlmin({collapseWhitespace: true}))
       .pipe(gulp.dest("pm/"))
-  );
-}
-
-function JsMinify(){
-  return(
-    gulp
-      .src("pm/js/*.js")
-      .pipe(minify())
-      .pipe(gulp.dest('pm/js'))
-  );
-}
-
-function JsConcat(){
-  return(
-    gulp
-      .src("pm/js/*min.js")
-      .pipe(concat('scripts.js'))
-      .pipe(gulp.dest('pm/js'))
   );
 }
 
@@ -55,4 +34,3 @@ function watch(){
 }
 
 exports.watch = watch;
-exports.build = series(JsMinify, JsConcat);
--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,30 @@
-var ButtonsContainer = document.getElementById("Buttons");
-var ButtonsContainer2 = document.getElementById("Buttons");
-var Buttons = ButtonsContainer.getElementsByClassName("create-company-change-type");
-var Buttons2 = ButtonsContainer2.getElementsByClassName("create-company-change-type-2");
-var Current = document.getElementsByClassName("create-company-change-type-active");
-var Current2 = document.getElementsByClassName("create-company-change-type-active-2");
-
-for(var i = 0; i < Buttons.length; i++){
-    Buttons[i].addEventListener("click", function(){
-    Current[0].className = Current[0].className.replace(" create-company-change-type-active", "");
-    this.className += " create-company-change-type-active";
-  });
-}
-
-for(var i = 0; i < Buttons2.length; i++){
-    Buttons2[i].addEventListener("click", function(){
-    Current2[0].className = Current2[0].className.replace(" create-company-change-type-active-2", "");
-    this.className += " create-company-change-type-active-2";
-  });
-}
+var myLink = document.getElementById('hidden-link');
+var myLink2 = document.getElementById('hidden-link-2');
+var myElement = document.getElementById('hidden-element');
+myLink.onclick = function(){myElement.style.transform = "translateX(0)";};
+myLink2.onclick = function(){myElement.style.transform = "translateX(-100%)";};
+var modal=document.getElementById("video-modal-1");
+var modal=document.getElementById("video-modal-2");
+var modal=document.getElementById("chat-modal-1");
+window.onclick=function(a){if(a.target==modal){modal.style.display="none";}};
+function ChangeImage1(){document.getElementById('ImagePost').src="img/company/post/1.png";} 
+function ChangeImage2(){document.getElementById('ImagePost').src="img/company/post/2.png";} 
+function ChangeImage3(){document.getElementById('ImagePost').src="img/company/post/3.png";} 
+function ChangeImage4(){document.getElementById('ImagePost').src="img/company/post/4.png";} 
+function ChangeLinkAuto(){document.getElementById('ChangeLink').href="create-company-auto-1.html";} 
+function ChangeLinkManual(){document.getElementById('ChangeLink').href="create-company-manual-1.html";} 
+function ChangeLinkPro(){document.getElementById('ChangeLink').href="create-company-pro-1.html";} 
+function ImagePreviewShow(){
+document.getElementById('ChangeImagePreviewHide').style.display = "block";
+document.getElementById('BlockCreateBlogger').style.display = "block";
+document.getElementById('BlockCreateSelf').style.display = "none";
+} 
+function ImagePreviewHide(){
+document.getElementById('ChangeImagePreviewHide').style.display = "none";
+document.getElementById('BlockCreateSelf').style.display = "block";
+document.getElementById('BlockCreateBlogger').style.display = "none";
+} 
+function openbox(pClass){
+var elems = document.getElementsByClassName(pClass);
+for(var i=0; i < elems.length; i++){var display = elems[i].style.display;if(display=='none')
+{elems[i].style.display='block';}else{elems[i].style.display='none';}}}
