76e16ddf6e50fdac947b01046b36d5594db403e9
Alexey Kosenkov <starko.web@gmail.com>
2021-01-26 21:59:11 +0300

    Delete modal.js

--- js/modal.js
+++
@@ -1,9 +0,0 @@
-var modal=document.getElementById("video-modal-1");
-var modal=document.getElementById("video-modal-2");
-var modal=document.getElementById("chat-modal-1");
-
-window.onclick=function(a){
-  if(a.target==modal){
-    modal.style.display="none";
-  }
-};
