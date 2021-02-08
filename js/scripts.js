7bb36dd533ad58bdb943abbab318268c1c30b896
Alexey Kosenkov <starko.web@gmail.com>
2021-02-09 01:00:27 +0300

    2.3.1
    3 этапа рк в авто режиме
    При создании рк "помощь"
    Шрифт и иконки теперь по ссылке (вместо подключения)
    Исправлена ошибка с js scroll.js

--- js/scripts.js
+++ js/scripts.js
@@ -37,10 +37,3 @@ function openbox(pClass){
 var elems = document.getElementsByClassName(pClass);
 for(var i=0; i < elems.length; i++){var display = elems[i].style.display;if(display=='none')
 {elems[i].style.display='block';}else{elems[i].style.display='none';}}}
-
-window.onscroll=function(){myFunction()};
-var navbar = document.getElementById("search");
-var sticky = navbar.offsetTop;
-function myFunction(){
-  if(window.pageYOffset >= sticky){navbar.classList.add("sticky")}else{navbar.classList.remove("sticky");}
-}
