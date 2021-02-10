60dba20a0fc6ae996758c504f593e3eb882f6c4c
Alexey Kosenkov <starko.web@gmail.com>
2021-02-10 16:06:37 +0300

    2.2.7
    5 сторис
    карточка блогера - выполнена задача

--- js/scripts.js
+++ js/scripts.js
@@ -25,20 +25,29 @@ document.getElementById('ChangeImagePreviewHide').style.display = "none";
 document.getElementById('BlockCreateSelf').style.display = "block";
 document.getElementById('BlockCreateBlogger').style.display = "none";
 } 
+function ChangeCofusTap(){
+  document.getElementById('ChangeFocus').placeholder = '15';
+  document.getElementById('ChangeImagePreviewHide').style.display = "none";
+} 
+function ChangeCofusFocus(){
+  document.getElementById('ChangeFocus').placeholder = '50';
+  document.getElementById('ChangeImagePreviewHide').style.display = "block";
+} 
 function openbox(pClass){
 var elems = document.getElementsByClassName(pClass);
 for(var i=0; i < elems.length; i++){var display = elems[i].style.display;if(display=='none')
 {elems[i].style.display='block';}else{elems[i].style.display='none';}}}
 
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("search");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
+function ShowMoreStory(){
+  var checkBox = document.getElementById("CampTypeMoreStory");
+  var text = document.getElementById("CampTypeMoreStoryOpened");
+  if(checkBox.checked == true){text.style.display = "block";}else{text.style.display = "none";}
+}
+function StoryPreviewShow(){
+  document.getElementById('ShowMoreStoryButtonOpened').style.display = "none";
+  document.getElementById('ShowMoreStoryButtonOpened').style.display = "block";
+} 
+function StoryPreviewHide(){
+  document.getElementById('ShowMoreStoryButtonOpened').style.display = "block";
+  document.getElementById('ShowMoreStoryButtonOpened').style.display = "none";
 }
---
+++ js/scroll.js
@@ -0,0 +1,5 @@
+
+window.onscroll=function(){myFunction()};
+var navbar = document.getElementById("search");
+var sticky = navbar.offsetTop;
+function myFunction(){if(window.pageYOffset >= sticky){navbar.classList.add("sticky")}else{navbar.classList.remove("sticky");}}
